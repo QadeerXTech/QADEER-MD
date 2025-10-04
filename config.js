4136d2f44c707be630266b786b87067172bd3903
QadeerXTech <yourqadeer5253@gmail.com>
2025-10-04 22:56:20 +0500

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORE SETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "IMMU~iaISwbYB#aATKYwy7vDTonEwPmZ4sw-PnRnhZMmBMuQfwJfNCA0s",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "QADEER-MD~rEkkFQRK#1FvQ7RGjk5mtCLC4g3z4k_cIGPt1WewRWZ4IMMeMUM4",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     CHATBOT: getConfig("CHATBOT") || "on", // on/off chat bot 
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "QADEER-MD",  // Bot's display name
